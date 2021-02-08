e282106a6911a1c0ae37d8aead1af38c8eb88cd3
Patrick Tumbucon <patricktumbucon@gmail.com>
2021-02-07 17:13:05 -0800

    Add copyright to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
+import { FaCopyright } from 'react-icons/fa'
 import styles from './Footer.module.css'
 
 export default function Footer(props) {
     return (
         <div className={styles.footer}>
             <p className="styles.message">Made with (✿◡‿◡)</p>
+            <br></br>
+            <p className="styles.message">
+                Copyright <FaCopyright /> 2021 Okizeme Gaming. All Rights Reserved
+            </p>
         </div>
     )
 }
