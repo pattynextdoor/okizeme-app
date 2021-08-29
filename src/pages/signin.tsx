83862c787d305e5062d4b5f6782bfd6bb6d9ca7a
Patrick Tumbucon <patricktumbucon@gmail.com>
2021-08-28 19:39:23 -0700

    Set up auth hub

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,7 +1,24 @@
 import { useEffect } from "react";
 import { useRouter } from 'next/router'
-import { Auth } from 'aws-amplify'
+import { Auth, Hub } from 'aws-amplify'
 import { withAuthenticator, AmplifySignUp } from "@aws-amplify/ui-react";
+
+async function updateUserWithDefaultAttributes() {
+    const user = await Auth.currentAuthenticatedUser()
+    const avatarString = 'https://avatars.dicebear.com/api/jdenticon/' + user.username + '.svg'
+    await Auth.updateUserAttributes(user, {
+        picture: avatarString
+    })
+}
+
+Hub.listen('auth', (data) => {
+    switch(data.payload.event) {
+        case 'signUp':
+            console.log('User signed up. Assigning default attributes...')
+
+            break;
+    }
+})
 
 const SignIn = () => {
     const router = useRouter()
